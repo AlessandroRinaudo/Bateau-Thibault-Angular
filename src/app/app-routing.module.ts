3eeb2c62917a091b11cff38981740414b31ea49b
Alessandro Rinaudo <>
2020-10-21 11:17:49 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,19 +11,16 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-<<<<<<< HEAD
   {
     path: 'vue2',
     loadChildren: () => import('./vue2/vue2.module').then( m => m.Vue2PageModule)
   },
-=======
 
   {
     path: 'recettes',
     loadChildren: () => import('./recettes/recettes.module').then( m => m.RecettesPageModule)
   },
   
->>>>>>> cd088798d2c680382f00824cd38fead126f08228
 ];
 
 @NgModule({
